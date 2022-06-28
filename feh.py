3a38992a520c096e81a0d9d2dd54ef7cf59c4db0
F-and-F <farzin.azimzade@protonmail.com>
2022-06-28 12:47:32 +0430

    change /home/farzin to $HOME

--- feh.py
+++ feh.py
@@ -2,20 +2,20 @@ import json
 import random
 import os
 
-with open("/home/farzin/Code/scripts/data.json") as data_file:
+with open("$HOME/Code/scripts/data.json") as data_file:
     data = json.load(data_file)
 
 if data['number'] == 3:
     rand = random.randint(1, 300)
-    os.system('feh --bg-scale /home/farzin/Pictures/Wallpapers/{}.jpg'.format(rand))
+    os.system('feh --bg-scale $HOME/Pictures/Wallpapers/{}.jpg'.format(rand))
     data["number"] = 0
     data["random"] = rand
 else:
     prev = data["random"]
-    os.system('feh --bg-scale /home/farzin/Pictures/Wallpapers/{}.jpg'.format(prev))
+    os.system('feh --bg-scale $HOME/Pictures/Wallpapers/{}.jpg'.format(prev))
     data["number"] += 1
 
-with open("/home/farzin/Code/scripts/data.json", "w") as write_file:
+with open("$HOME/Code/scripts/data.json", "w") as write_file:
     json.dump(data, write_file)
 
 
